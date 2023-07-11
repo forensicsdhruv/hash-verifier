8bfde650938123be17986815bfe48b08188188cf
Dhruv Sharma <134591919+forensicsdhruv@users.noreply.github.com>
2023-07-11 16:16:47 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,9 @@
+
+while True:
+    a = input("Enter your first hash vlue ")
+    b = input("Enter your second hash value ")
+    if a == b :
+        print("Hash Verified ")
+    else :
+        print("Hash not Matched")
+
